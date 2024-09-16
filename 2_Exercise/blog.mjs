a15eacc79ed4b1067d46a0bb26bc82832fff99ed
Kelly J <[kelly4410@gmail.com]>
2024-09-16 09:11:29 -0600

    complete exercise 1 and 2

---
+++ 2_Exercise/blog.mjs
@@ -0,0 +1,8 @@
+// Create blog.mjs that imports the Post class. Create an instance, 
+// and call the publish method to simulate posting.
+
+import Post from './Post.mjs';
+
+const myPost = new Post("My first post.");
+myPost.publish();
+
