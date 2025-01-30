9a0f82b61733cdc4459d0eb3e2a534a42417b10c
Kelly J <[kelly4410@gmail.com]>
2025-01-30 10:03:43 -0700

    create theme

--- 4_Exercise/app.js
+++ 4_Exercise/app.js
@@ -3,3 +3,17 @@
 // setDarkTheme set and log the theme being set to the console.
 // Create app.js. Implement an async function, loadConfig, that dynamically imports the theme.mjs and decides which 
 // theme function to call based on the current hour (e.g., light theme before 6 PM, dark theme afterward).
+
+async function loadConfig() {
+    const hour = new Date().getHours(); //this gets the time right now then extracts the exact number of hours
+
+    const moduleTheme = await import('./theme.mjs'); //imports the theme.mjs, await because async
+
+    if (hour < 18) {
+        moduleTheme.setLightTheme(); //if the time is before 6pm, set light theme
+    } else {
+        moduleTheme.setDarkTheme(); //if the time is after 6pm, set dark theme
+    }
+}
+
+loadConfig(); //call function to apply theme
---
+++ 4_Exercise/theme.mjs
@@ -0,0 +1,11 @@
+export function setLightTheme() {
+    console.log("Light theme applied.");
+    document.body.style.backgroundColor = "#f0f6f7"; //light blue background
+    document.body.style.color = "#000000"; //black font
+}
+
+export function setDarkTheme() {
+    console.log("Dark theme applied.");
+    document.body.style.backgroundColor = "#1e1e1e"; //dark gray background
+    document.body.style.color = "#ffffff"; //white font
+}
